e1ce4d69cd42ec0f6aab468e201635fdeff21959
m-takahashi <masan3821031@gmail.com>
2020-08-10 11:09:34 +0900

    router

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,16 +1,21 @@
-import { createStore as reduxCreateStore, combineReducers } from "redux";
+import {
+	createStore as reduxCreateStore,
+	combineReducers,
+	applyMiddleware,
+} from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 
 // Import reducers
 // import { ProductsReducer } from "../users/reducers";
 import { UsersReducer } from "../users/reducers";
 
 // createStoreの再定義 - historyを引数で受け、connected-react-routerの利用を抽象化
-export default function createStore() {
+export default function createStore(history) {
 	return reduxCreateStore(
-		// オリジナル createStore の別名
 		combineReducers({
-			// products: ProductsReducer,
+			router: connectRouter(history),
 			users: UsersReducer,
-		})
+		}),
+		applyMiddleware(routerMiddleware(history))
 	);
 }
