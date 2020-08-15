c0d0b5454e6202854597cab7bd6ca8a73240e96a
m-takahashi <masan3821031@gmail.com>
2020-08-15 11:40:20 +0900

    usersSelector/re-ducks

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
+import { signInAction } from "../reducks/users/actions";
 
 const Login = () => {
 	const dispatch = useDispatch();
+
 	return (
 		<div>
 			<h2>ログイン</h2>
-			<button onClick={() => dispatch(push("/"))}>ログインする</button>
+			<button
+				onClick={() => {
+					dispatch(signInAction({ uid: "00001", username: "torahack" }));
+					dispatch(push("/"));
+				}}
+			>
+				ログインする
+			</button>
 		</div>
 	);
 };
