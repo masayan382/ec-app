2115c6a9c19306103ff60ce6ae68690b22767da9
m-takahashi <masan3821031@gmail.com>
2020-08-15 14:11:50 +0900

    react-thunk

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { getUserId } from "../reducks/users/selectors";
+import { getUserName } from "../reducks/users/selectors";
 import { useSelector } from "react-redux";
 
 const Home = () => {
 	const selector = useSelector((state) => state);
 	const uid = getUserId(selector);
+	const username = getUserName(selector);
+
 	return (
 		<div>
 			<h2>Home</h2>
-			<p>{uid}</p>
+			<p>ユーザーID:{uid}</p>
+			<p>ユーザー名：{username}</p>
 		</div>
 	);
 };
