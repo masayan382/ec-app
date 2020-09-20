5cec04b7dd64ccd41d1a1153419277e5b37eea07
m-takahashi <masan3821031@gmail.com>
2020-09-20 17:10:07 +0900

    注文履歴

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router";
 import {
 	CartList,
 	OrderConfirm,
+	OrderHistory,
 	ProductDetail,
 	ProductList,
 	ProductEdit,
@@ -26,6 +27,7 @@ const Router = () => {
 
 				<Route exact path={"/cart"} component={CartList} />
 				<Route exact path={"/order/confirm"} component={OrderConfirm} />
+				<Route exact path={"/order/history"} component={OrderHistory} />
 			</Auth>
 		</Switch>
 	);
